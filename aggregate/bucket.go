12524aa76341d2f8efb8a76e3d25d0232ccbd988
Josh Gummersall <josh@standardlabs.dev>
2020-03-24 17:05:05 -0700

    Compare at level 20

--- aggregate/bucket.go
+++ aggregate/bucket.go
@@ -135,7 +135,7 @@ func Bucket(ctx context.Context, in, out string) error {
 		wc := object.NewWriter(ctx)
 		writer := csv.NewWriter(wc)
 		for _, point := range points {
-			err = writer.Write([]string{point.timestamp, point.cellID.ToToken(), point.status})
+			err = writer.Write([]string{point.timestamp, point.cellID.Parent(20).ToToken(), point.status})
 			if err != nil {
 				return err
 			}
